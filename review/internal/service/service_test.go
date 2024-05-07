bf0d0bdf3a0ffa4217b0cc4f1693317044759e80
Estremadures <esibajo@gmail.com>
2024-05-07 17:02:52 +0200

    Add test for Get Coupons

--- review/internal/service/service_test.go
+++ review/internal/service/service_test.go
@@ -91,3 +91,38 @@ func TestService_CreateCoupon(t *testing.T) {
 		})
 	}
 }
+
+func TestService_GetCoupons(t *testing.T) {
+	type fields struct {
+		repo Repository
+	}
+	type args struct {
+		discount       int
+		code           string
+		minBasketValue int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{"Apply 10%", fields{memdb.New()}, args{10, "Superdiscount", 55}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Service{
+				repo: tt.fields.repo,
+			}
+
+			s.CreateCoupon(tt.args.discount, tt.args.code, tt.args.minBasketValue)
+			coupons, err := s.GetCoupons([]string{tt.args.code})
+			if len(coupons) > 1 {
+				t.Errorf("GetCoupons() should return one coupon")
+			}
+			if err != nil {
+				t.Errorf("ApplyCoupon() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
