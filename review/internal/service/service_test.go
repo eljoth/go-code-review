4f556c614fd03e1b5df18b5fc9550aceac68a3ef
Guillem Bonet <guillem.bonet+private@gmail.com>
2024-06-26 18:17:44 +0200

    extend tests to fit all cases

--- review/internal/service/service_test.go
+++ review/internal/service/service_test.go
@@ -30,7 +30,8 @@ func TestNew(t *testing.T) {
 
 func TestService_ApplyCoupon(t *testing.T) {
 	type fields struct {
-		repo repository.Repository
+		repo    repository.Repository
+		coupons []entity.Coupon
 	}
 	type args struct {
 		basket entity.Basket
@@ -42,12 +43,62 @@ func TestService_ApplyCoupon(t *testing.T) {
 		args    args
 		wantB   *entity.Basket
 		wantErr bool
-	}{}
+	}{
+		{
+			"Apply 10%",
+			fields{memdb.New(), []entity.Coupon{{ID: "1", Code: "10_off", Discount: 10, MinBasketValue: 3}}},
+			args{entity.Basket{Value: 100}, "10_off"},
+			&entity.Basket{Value: 100, AppliedDiscount: 10, ApplicationSuccessful: true},
+			false,
+		},
+		{
+			"Apply 50%",
+			fields{memdb.New(), []entity.Coupon{{ID: "1", Code: "50_off", Discount: 50, MinBasketValue: 3}}},
+			args{entity.Basket{Value: 100}, "50_off"},
+			&entity.Basket{Value: 100, AppliedDiscount: 50, ApplicationSuccessful: true},
+			false,
+		},
+		{
+			"Fail to apply 10%",
+			fields{memdb.New(), []entity.Coupon{{ID: "1", Code: "10_off", Discount: 10, MinBasketValue: 200}}},
+			args{entity.Basket{Value: 100}, "10_off"},
+			&entity.Basket{Value: 100, AppliedDiscount: 0, ApplicationSuccessful: false},
+			false,
+		},
+		{
+			"Unexisting coupon",
+			fields{memdb.New(), nil},
+			args{entity.Basket{Value: 100}, "10_off"},
+			nil,
+			true,
+		},
+		{
+			"Negative value basket",
+			fields{memdb.New(), []entity.Coupon{{ID: "1", Code: "10_off", Discount: 10, MinBasketValue: 0}}},
+			args{entity.Basket{Value: -10}, "10_off"},
+			nil,
+			true,
+		},
+		{
+			"Zero value basket",
+			fields{memdb.New(), []entity.Coupon{{ID: "1", Code: "10_off", Discount: 10, MinBasketValue: 0}}},
+			args{entity.Basket{Value: 0}, "10_off"},
+			&entity.Basket{Value: 0, AppliedDiscount: 0, ApplicationSuccessful: true},
+			false,
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := Service{
 				repo: tt.fields.repo,
 			}
+			for _, c := range tt.fields.coupons {
+				err := s.repo.Save(c)
+				if err != nil {
+					t.Errorf("failed to save coupon: %v", c)
+				}
+			}
+
 			gotB, err := s.ApplyCoupon(tt.args.basket, tt.args.code)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ApplyCoupon() error = %v, wantErr %v", err, tt.wantErr)
@@ -62,7 +113,8 @@ func TestService_ApplyCoupon(t *testing.T) {
 
 func TestService_CreateCoupon(t *testing.T) {
 	type fields struct {
-		repo repository.Repository
+		repo            repository.Repository
+		existingCoupons []entity.Coupon
 	}
 	type args struct {
 		discount       int
@@ -70,20 +122,63 @@ func TestService_CreateCoupon(t *testing.T) {
 		minBasketValue int
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   any
+		name    string
+		fields  fields
+		args    args
+		wantC   *entity.Coupon
+		wantErr bool
 	}{
-		{"Apply 10%", fields{memdb.New()}, args{10, "Superdiscount", 55}, nil},
+		{
+			"Create 10%",
+			fields{memdb.New(), nil},
+			args{10, "Superdiscount", 55},
+			&entity.Coupon{Code: "Superdiscount", Discount: 10, MinBasketValue: 55},
+			false,
+		},
+		{
+			"Create 50%",
+			fields{memdb.New(), []entity.Coupon{{ID: "1", Code: "Superdiscount", Discount: 10, MinBasketValue: 200}}},
+			args{50, "Superdiscount50", 100},
+			&entity.Coupon{Code: "Superdiscount50", Discount: 50, MinBasketValue: 100},
+			false,
+		},
+		{
+			"Already existing error",
+			fields{memdb.New(), []entity.Coupon{{ID: "1", Code: "Superdiscount", Discount: 10, MinBasketValue: 200}}},
+			args{10, "Superdiscount", 55},
+			nil,
+			true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := Service{
 				repo: tt.fields.repo,
 			}
+			for _, c := range tt.fields.existingCoupons {
+				err := s.repo.Save(c)
+				if err != nil {
+					t.Errorf("failed to save coupon: %v", c)
+					return
+				}
+			}
 
-			s.CreateCoupon(tt.args.discount, tt.args.code, tt.args.minBasketValue)
+			gotC, err := s.CreateCoupon(tt.args.discount, tt.args.code, tt.args.minBasketValue)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateCoupon() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if err == nil {
+				if gotC.ID == "" {
+					t.Errorf("CreateCoupon() returned coupon with empty ID")
+				}
+				gotC.ID = ""
+			}
+
+			if !reflect.DeepEqual(gotC, tt.wantC) {
+				t.Errorf("ApplyCoupon() gotC = %v, want %v", gotC, tt.wantC)
+			}
 		})
 	}
 }
